87f25e718dddf1eca7eb57fd6e94f0ab107f732d
Jason Ray <jayray.net@gmail.com>
2023-12-13 12:38:52 -0500

    bump v1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='is-numeric',
-      version='0.0.1',
+      version='1.0.0',
       author='Mighty Pulpo',
       author_email='jayray.net@gmail.com',
       description='The missing Python method to determine if a value is numeric',
